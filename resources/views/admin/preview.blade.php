5b509509c925a9db48902e2932c331e6c70befcf
ballack <rodrigue-dev>
2023-11-27 12:25:30 +0100

    ertres

--- resources/views/admin/preview.blade.php
+++ resources/views/admin/preview.blade.php
@@ -7,8 +7,8 @@
         @for($i=0; $i<11;$i++)
             <div class="col-md-2 col-sm-4 col-6 col-xl-2 grid-margin stretch-card">
                 <div class="card text-black" style="background-color: darkslategrey; min-height: 150px;background-repeat: no-repeat;
-                    background-size:contain;
-                    background-position-x: center; background-image: url('{{asset('admin/images/Logo_transparent_USDC_20231125_183246_0000.svg')}}')">
+                    background-size: 50px;
+                    background-position-x: center;background-position-y: center; background-image: url('{{asset('admin/images/Logo_transparent_USDC_20231125_183246_0000.svg')}}')">
                     <div class="card_body">
                         <div>
                             <span class="h3 text-start">L{{$i}}</span>
